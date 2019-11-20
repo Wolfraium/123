e11e6b24305a9f2b0adb9ea5496f4b3c72642843
Macbeth <55477198+Wolfraium@users.noreply.github.com>
2019-11-20 11:50:06 +0800

    Add files via upload

--- Hello.py
+++ Hello.py
@@ -1,1 +1,22 @@
-#
+temperature = []
+with open('D:/Python/txt.txt') as t:
+    for line in t: 
+        temperature.append(int(line))
+
+print(temperature)
+
+avg = sum(temperature)/len(temperature)
+
+temperature_deviation = []
+
+for t in temperature:
+    temperature_deviation.append(t - avg)
+
+print(avg)
+
+with open('D:/Python/average_temperature.txt', 'w') as aver_tem:
+    aver_tem.write('%.2f' % avg)
+
+with open('D:/Python/temperature_deviation.txt', 'w') as temp_dev:
+    for t in temperature_deviation:
+        temp_dev.write('%.2f\n' % t)
